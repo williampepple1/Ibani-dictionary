3528a51f32745e70686e0a752a329d6f01bf43ed
William Pepple <williampepple1@gmail.com>
2024-08-22 18:52:51 +0100

    Correct Deployment

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ app = FastAPI()
 # Add CORS middleware to the app
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["https://williampepple1.github.io/ibani-frontend/"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
+    allow_methods=["GET"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
 
